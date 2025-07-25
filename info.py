c7fae8583c99697afeff8ccc8801f6d0f6d82fb8
herobotk <herobot642@gmail.com>
2025-07-26 02:49:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://envs.sh/0oC.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.postimg.cc/G3JGbqmP/IMG-20250723-085849.jpg')).split()
 
 
 # Admins & Users
